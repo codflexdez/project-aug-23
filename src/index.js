61a5d466e8297c1a2eab7c542627b3d7b68a4305
rompo80 <romanworld@gmail.com>
2023-09-02 19:35:44 -0400

    uploaded npm i fontawesome-4.7 commented out the fontawesome cdn on index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import '../src/css/main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../src/css/main.css";
+import "../node_modules/fontawesome-4.7/css/font-awesome.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
